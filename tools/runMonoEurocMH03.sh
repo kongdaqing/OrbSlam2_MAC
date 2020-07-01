0b67712d05478fbdec87d407e76c90654c9923c0
kongdaqing <kongdaqing@zerozero.cn>
2020-07-01 22:38:46 +0800

    remove tools for personal convenience not good to others

--- tools/runMonoEurocMH03.sh
+++
@@ -1,1 +0,0 @@
-./Examples/Monocular/mono_euroc Vocabulary/ORBvoc.txt Examples/Monocular/EuRoC.yaml ../DataSet/euroc/MH_03/mav0/cam0/data Examples/Monocular/EuRoC_TimeStamps/MH03.txt 
--- tools/runStereoEurocMH03.sh
+++
@@ -1,1 +0,0 @@
-./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml ../DataSet/euroc/MH_03/mav0/cam0/data ../DataSet/euroc/MH_03/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/MH03.txt 
